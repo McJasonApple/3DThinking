653dd64ff18d466ab5f9f54e2112c0be1b560df0
Annika <annika.latz@web.de>
2021-12-11 10:13:40 +0100

    Set up pubspec and projectstructure

---
+++ threed_thinking/lib/ui/homepage_screen.dart
@@ -0,0 +1,20 @@
+// start screen
+
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Welcome to 3D Thinking'),
+    );
+  }
+}
